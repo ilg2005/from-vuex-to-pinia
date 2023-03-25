48bf8fbf5aae8a2f93ed84762b10b56543b4f7a4
ilg2005 <igoldfarb10@gmail.com>
2023-03-25 16:44:38 +0300

    actions

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import EventService from '../services/EventService.js'
 
 export const useEventStore = defineStore('EventStore', {
     state: () => ({
@@ -7,5 +8,16 @@ export const useEventStore = defineStore('EventStore', {
     }),
     getters: {
         numberOfEvents: state => state.events.length,
+    },
+    actions: {
+        fetchEvents() {
+            return EventService.getEvents()
+                .then(response => {
+                    this.events = response.data
+                })
+                .catch(error => {
+                    throw error
+                })
+        },
     }
 });
